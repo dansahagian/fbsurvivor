5d1fc411ebb8085176822161aedfe8bc0dc3cb55
Dan Sahagian <dan@dansahagian.com>
2018-08-30 10:25:04 -0700

    initial commit

---
+++ db.py
@@ -0,0 +1,218 @@
+import os
+import secrets
+import string
+import datetime
+import psycopg2
+
+
+def query_db(sql):
+    db = 'survivor'
+    user = 'sv_user'
+    pw = os.environ['PG_DB']
+    host = 'localhost'
+
+    try:
+        cnxn = psycopg2.connect(host=host, database=db, user=user, password=pw)
+        cursor = cnxn.cursor()
+        cursor.execute(sql)
+        return cursor.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if cnxn is not None:
+            cnxn.close()
+
+
+def insert_db(sql, values):
+    db = 'survivor'
+    user = 'sv_user'
+    pw = os.environ['PG_DB']
+    host = 'localhost'
+
+    try:
+        cnxn = psycopg2.connect(host=host, database=db, user=user, password=pw)
+        cursor = cnxn.cursor()
+        cursor.execute(sql, values)
+        cnxn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
+    finally:
+        if cnxn is not None:
+            cnxn.close()
+
+
+def username_available(username):
+    sql = """SELECT username from users"""
+
+    if username not in [x[0] for x in query_db(sql)]:
+        return True
+    return False
+
+
+def add_user(username, email):
+    char_set = string.ascii_lowercase + string.digits
+    link = ''.join(secrets.choice(char_set) for _ in range(44))
+
+    sql = """
+          INSERT INTO users (username, email, link)
+          VALUES (%s, %s, %s);
+          """
+    values = (username, email, link)
+
+    insert_db(sql, values)
+    return link
+
+
+def add_user_picks(link, year):
+    user_id = get_user_id(link)
+
+    for i in range(0, 17):
+        sql = """
+              INSERT INTO picks (user_id, year, week, team)
+              VALUES (%s, %s, %s, %s);
+              """
+        values = (user_id, year, i + 1, '--')
+        insert_db(sql, values)
+
+
+def add_paid_status(link, year):
+    sql = """
+          INSERT INTO paid (user_id, year, paid, result)
+          VALUES (%s, %s, %s, %s);
+          """
+    values = (get_user_id(link), year, False, 'A')
+    insert_db(sql, values)
+
+
+def get_user_picks(link, year):
+    sql = """
+          SELECT p.week, p.team, p.result, l.lock_date
+          FROM picks p
+          JOIN locks l ON p.year = l.year AND p.week = l.week
+          JOIN users u ON u.id = p.user_id
+          WHERE u.link = '%s'
+          AND p.year = %s
+          ORDER BY p.week
+          """ % (link, year)
+
+    data = query_db(sql)
+    picks = []
+    for row in data:
+        dl = row[3].strftime("%m-%d %I:%M PST")
+        if datetime.datetime.now() > row[3]:
+            picks.append([row[0], row[1], row[2], dl, False])
+        else:
+            picks.append([row[0], row[1], row[2], dl, True])
+    return picks
+
+
+def get_years():
+    sql = """SELECT year from years order by year desc"""
+    return [x[0] for x in query_db(sql)]
+
+
+def year_locked(year):
+    sql = """SELECT lock FROM years WHERE year = %s""" % (year)
+    return query_db(sql)[0][0]
+
+
+def week_locked(year, week):
+    sql = """
+          SELECT lock_date FROM locks
+          WHERE year = %s AND week = %s
+          """ % (year, week)
+
+    if datetime.datetime.now() > query_db(sql)[0][0]:
+        return True
+    return False
+
+
+def valid_link(link):
+    sql = """SELECT link from users"""
+
+    if link in [x[0] for x in query_db(sql)]:
+        return True
+    return False
+
+
+def get_user_id(link):
+    sql = """SELECT id from users WHERE link = '%s'""" % (link)
+    return query_db(sql)[0][0]
+
+
+def get_team_choices(link, year, week):
+    sql = """
+          SELECT team FROM teams
+          WHERE year = %s AND bye_week != %s
+          """ % (year, week)
+    teams = set([x[0] for x in query_db(sql)])
+
+    sql = "SELECT team FROM picks WHERE year = %s" % (year)
+    used = set([x[0] for x in query_db(sql)])
+
+    return sorted(list(teams - used) + ['--'])
+
+
+def get_current_pick(link, year, week):
+    sql = """
+          SELECT team from picks
+          WHERE user_id = %s AND year = %s AND week = %s
+          """ % (get_user_id(link), year, week)
+
+    return query_db(sql)[0][0]
+
+
+def update_pick(link, year, week, pick):
+    sql = """
+          UPDATE picks
+          SET team = %s
+          WHERE user_id = %s AND year = %s and week = %s
+          """
+    values = (pick, get_user_id(link), year, week)
+
+    insert_db(sql, values)
+
+
+def get_board(year):
+    sql = """
+          SELECT u.id, u.username, p.paid, p.result
+          FROM users u
+          JOIN paid p ON u.id = p.user_id
+          WHERE p.year = %s
+          ORDER BY p.result, u.username, p.paid
+          """ % (year)
+
+    rows = query_db(sql)
+
+    data = []
+    for row in rows:
+        sql = """
+              SELECT p.team, p.result
+              FROM picks p
+              JOIN locks l ON l.week = p.week AND l.year = p.year
+              WHERE user_id = %s AND p.year = %s AND p.week <=
+              (SELECT max(week) FROM locks
+               WHERE CURRENT_TIMESTAMP > lock_date AND year = %s
+              )
+              ORDER BY p.week ASC
+              """ % (row[0], year, year)
+
+        picks = query_db(sql)
+        wins = len([x[1] for x in picks if x[1] == 'W'])
+        loss = len([x[1] for x in picks if x[1] == 'L'])
+
+        line = [row[1], row[2], row[3], wins, loss, picks]
+        data.append(line)
+
+        data.sort(key=lambda x: x[0])
+        data.sort(key=lambda x: x[1], reverse=True)
+        data.sort(key=lambda x: x[3], reverse=True)
+        data.sort(key=lambda x: x[4])
+        data.sort(key=lambda x: x[2])
+
+    return data
+
+
+def get_current_year():
+    return query_db('SELECT year from current')[0][0]
---
+++ fbsurvivor.py
@@ -0,0 +1,132 @@
+import os
+from flask import Flask, flash, redirect, request, abort
+from flask import render_template as rt
+from flask import send_from_directory as sfd
+import db
+
+
+app = Flask(__name__)
+app.secret_key = os.environ['FLASK_KEY']
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return rt('index.html')
+
+
+@app.route('/survive', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return rt('signup.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email'].lower()
+
+        if len(username) > 20:
+            flash('Username is greater than 20 characters! Choose again.')
+            return redirect('/survive')
+
+        if db.username_available(username):
+            link = db.add_user(username, email)
+            return redirect('/%s' % (link))
+
+        flash('Username already exists! Pick another!')
+        return redirect('/survive')
+
+
+@app.route('/<link>', methods=['GET'])
+def user(link):
+    if db.valid_link(link):
+        data = db.get_board(db.get_current_year())
+        return rt('user.html', years=db.get_years(), link=link, data=data)
+    else:
+        abort(404)
+
+
+@app.route('/<link>/previous/<year>', methods=['GET'])
+def previous(link, year):
+    if db.valid_link(link):
+        data = db.get_board(year)
+        return rt('board.html', link=link, year=year, data=data,
+                  years=db.get_years())
+    else:
+        abort(404)
+
+
+@app.route('/<link>/<year>', methods=['GET'])
+def picks(link, year):
+    if db.valid_link(link):
+        picks = db.get_user_picks(link, year)
+        wins = len([x[2] for x in picks if x[2] == 'W'])
+        loss = len([x[2] for x in picks if x[2] == 'L'])
+        return rt('picks.html', link=link, year=year,
+                  lock=db.year_locked(year), picks=picks, ws=wins, ls=loss)
+    else:
+        abort(404)
+
+
+@app.route('/<link>/<year>/<week>', methods=['GET', 'POST'])
+def pick(link, year, week):
+    if db.valid_link(link):
+        if request.method == 'GET':
+            if db.week_locked(year, week):
+                flash('Week %s is locked! Cannot Edit!' % (week))
+                return redirect('/%s/%s' % (link, year))
+            teams = db.get_team_choices(link, year, week)
+            pick = db.get_current_pick(link, year, week)
+            return rt('pick.html', link=link, year=year, week=int(week),
+                      pick=pick, c=teams)
+
+        if request.method == 'POST':
+            picks = [x[1] for x in db.get_user_picks(link, year)]
+            pick = request.form['pick']
+
+            if pick in picks and pick != '--':
+                flash('Pick NOT updated! %s already used.' % (pick))
+            else:
+                db.update_pick(link, year, week, pick)
+                flash('Pick updated! Week %s: %s' % (week, pick))
+                return redirect('/%s/%s' % (link, year))
+    else:
+        abort(404)
+
+
+@app.route('/<link>/<year>/play', methods=['GET'])
+def play_year(link, year):
+    if db.valid_link(link):
+        db.add_user_picks(link, year)
+        db.add_paid_status(link, year)
+        flash('You are playing in the %s league. Good luck!' % (year))
+        return redirect('/%s' % (link))
+    else:
+        abort(404)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    root = os.path.join(app.root_path, 'static')
+    return sfd(root, 'favicon.ico')
+
+
+@app.route('/css/main.css')
+def css():
+    root = os.path.join(app.root_path, 'static/css')
+    return sfd(root, 'main.css')
+
+
+@app.route('/fonts/OpenSans-Regular.eot')
+def font_eot():
+    root = os.path.join(app.root_path, 'static/fonts')
+    return sfd(root, 'OpenSans-Regular.eot')
+
+
+@app.route('/fonts/OpenSans-Regular.ttf')
+def font_ttf():
+    root = os.path.join(app.root_path, 'static/fonts')
+    return sfd(root, 'OpenSans-Regular.ttf')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return rt('404.html'), 404
