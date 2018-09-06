8520e8ebf43744166f64898bb6422ddbaf7b9db2
Dan Sahagian <dan@dansahagian.com>
2018-09-05 17:37:59 -0700

    makes username bold and adds email reminder

---
+++ reminder.py
@@ -0,0 +1,81 @@
+import os
+import smtplib
+import datetime
+import db
+
+from email.mime.text import MIMEText
+
+WHO_FROM = os.environ['WHO_FROM']
+SMTP_SER = os.environ['SMTP_SER']
+USERNAME = os.environ['USERNAME']
+PASSWORD = os.environ['PASSWORD']
+
+
+def get_players():
+    sql = """
+          SELECT email
+          FROM users u
+          JOIN paid p ON p.user_id = u.id
+          WHERE p.year = (SELECT year FROM current)
+          """
+
+    return [x[0] for x in db.query_db(sql)]
+
+
+def get_players_without_picks():
+    sql = """
+          SELECT u.email, pk.team
+          FROM users u
+          JOIN paid pd ON pd.user_id = u.id
+          JOIN picks pk ON pk.user_id = u.id
+          WHERE pd.year = (SELECT year FROM current)
+          AND pk.year = (SELECT year FROM current)
+          AND pk.week = (SELECT min(week) FROM locks WHERE
+                         lock_date > CURRENT_TIMESTAMP)
+          AND pk.team = '--'
+          """
+
+    return [x[0] for x in db.query_db(sql)]
+
+
+def get_lock_date():
+    sql = """
+          SELECT lock_date
+          FROM locks
+          WHERE year = (SELECT year FROM current)
+          """
+    current_date = datetime.datetime.now()
+
+    return min([x[0] for x in db.query_db(sql) if x[0] > current_date])
+
+
+def send_email(subject, who_to, message):
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = WHO_FROM
+    msg['To'] = WHO_FROM
+
+    cnxn = smtplib.SMTP_SSL(SMTP_SER)
+    cnxn.login(USERNAME, PASSWORD)
+    try:
+        cnxn.sendmail(WHO_FROM, who_to, msg.as_string())
+    finally:
+        cnxn.quit()
+
+
+def main():
+
+    lock_date = get_lock_date().strftime("%m-%d %I:%M %p PST")
+    subject = 'Survivor Picks Reminder'
+    message = "Picks are due by %s PST!" % lock_date
+    week_day = datetime.datetime.today().weekday()
+    if week_day == 3:
+        who_to = get_players_without_picks()
+    else:
+        who_to = get_players()
+
+    send_email(subject, who_to, message)
+
+
+if __name__ == '__main__':
+    main()
